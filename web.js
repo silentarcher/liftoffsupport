a42ac09e87fdd9c520e99608d25697af65d5c3e7
gregstallings <gregstallings@gmail.com>
2013-01-21 18:42:06 -0800

    Create node.js server

---
+++ web.js
@@ -0,0 +1,43 @@
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var port = process.env.PORT || 5000;
+
+http.createServer(function (request, response) {
+  console.log('Request starting...');
+
+  var filePath = '.' + request.url;
+  if (filePath == './')
+    filePath = './index.html';
+
+  var extname = path.extname(filePath);
+  var contentType = 'text/html';
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+  }
+
+  path.exists(filePath, function(exists) {
+    if (exists) {
+      fs.readFile(filePath, function(error, content) {
+        if (error) {
+          response.writeHead(500);
+          response.end();
+        } else {
+          response.writeHead(200, { 'Content-Type': contentType });
+          response.end(content, 'utf-8');
+        }
+      });
+    } else {
+      response.writeHead(404);
+      response.end();
+    }
+  });
+
+}).listen(port);
+
+console.log('Server running');
