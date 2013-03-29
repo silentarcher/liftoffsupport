427de6d72794a8d264ddf13e5fbe90d58b9db2d3
unknown <gregstallings@gmail.com>
2013-03-29 01:41:02 -0700

    Modify scroll

--- js/scroll.js
+++ js/scroll.js
@@ -1,21 +1,21 @@
 /*! scroll.js */
 
-$(function() {
+;$(function() {
   // Set handlers on section scrollers
   $('a[href^="#section-"]').click(function() {
-    var that = $('a[name="' + $(this).attr('href').substring(1, $(this).attr('href').length) + '"]');
+    var that = $('a[name="' + $(this).attr('href').substring(1, $(this).attr('href').length) + '"]')
 
     // Chrome and Safari
     $('body').animate({
       scrollTop: ($(that).offset().top - 48)
-    }, 800, 'swing');
+    }, 800, 'swing')
 
     // Firefox and IE
     $('html').animate({
       scrollTop: ($(that).offset().top - 34)
-    }, 800, 'swing');
+    }, 800, 'swing')
 
     // Cancel default behavior (navigation to #name) because this was glitchy in the browser
-    return false;
-  });
+    return false
+  })
 });
