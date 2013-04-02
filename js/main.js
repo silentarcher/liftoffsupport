157ea035903e0f04bc5079b3c5f867d048320dee
gregstallings <gregstallings@gmail.com>
2013-04-02 00:54:11 -0700

    Fix scroll

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-// (c) 2013 Liftoff Computer Services, LLC
-// http://liftoffsupport.com
+/*!
+ * main.js
+ *
+ * Copyright (c) 2013 Liftoff Computer Services, LLC
+ * http://liftoffsupport.com
+ *
+ */
 
 ;(function($, window, undefined) {
 
@@ -16,15 +21,13 @@
     render: function() {
       var source = $(this.template).html()
       var template = Handlebars.compile(source)
-      var html = template({})
+      var body = template({})
 
       var nav = new NavPartial().render({isHome: true}).$el
       var footer = new FooterPartial().render().$el
 
-      $('#container').html('')
-      $('#container').append(nav)
-      $('#container').append(html)
-      $('#container').append(footer)
+      var raw = nav[0].innerHTML + body + footer[0].innerHTML
+      $('#container').html(raw)
     }
   })
 
@@ -37,15 +40,13 @@
     render: function() {
       var source = $(this.template).html()
       var template = Handlebars.compile(source)
-      var html = template({})
+      var body = template({})
 
       var nav = new NavPartial().render().$el
       var footer = new FooterPartial().render().$el
 
-      $('#container').html('')
-      $('#container').append(nav)
-      $('#container').append(html)
-      $('#container').append(footer)
+      var raw = nav[0].innerHTML + body + footer[0].innerHTML
+      $('#container').html(raw)
     }
   })
 
@@ -58,15 +59,13 @@
     render: function() {
       var source = $(this.template).html()
       var template = Handlebars.compile(source)
-      var html = template({})
+      var body = template({})
 
       var nav = new NavPartial().render().$el
       var footer = new FooterPartial().render().$el
 
-      $('#container').html('')
-      $('#container').append(nav)
-      $('#container').append(html)
-      $('#container').append(footer)
+      var raw = nav[0].innerHTML + body + footer[0].innerHTML
+      $('#container').html(raw)
     }
   })
 
@@ -113,36 +112,68 @@
     routes: {
       '!/locations': 'locations',
       '!/pricing': 'pricing',
-      'section-1': 'home',
-      'section-2': 'home',
-      'section-3': 'home',
-      'section-4': 'home',
-      'section-5': 'home',
+      'small-business': 'smallBusiness',
+      'home-support': 'homeSupport',
+      'home-networking': 'homeNetworking',
+      'sales': 'sales',
+      'home-theater': 'homeTheater',
       '': 'home'
     },
     home: function() {
-      console.log('home route')
       var homeView = new HomeView()
       homeView.render()
     },
     locations: function() {
-      console.log('locations route')
       var locationsView = new LocationsView()
       locationsView.render()
       window.scroll(0, 0)
     },
     pricing: function() {
-      console.log('pricing route')
       var pricingView = new PricingView()
       pricingView.render()
       window.scroll(0, 0)
+    },
+    smallBusiness: function() {
+      scrollHome('small-business')
+    },
+    homeSupport: function() {
+      scrollHome('home-support')
+    },
+    homeNetworking: function() {
+      scrollHome('home-networking')
+    },
+    sales: function() {
+      scrollHome('sales')
+    },
+    homeTheater: function() {
+      scrollHome('home-theater')
     }
   })
+
+  function scrollHome(section) {
+    // Render the home page if it's not already
+    if (!$('.nav .left').length) {
+      var homeView = new HomeView()
+      homeView.render()
+    }
+
+    // Scroll to the requested section
+    var that = $('div .' + section)
+
+    // Chrome and Safari
+    $('body').animate({
+      scrollTop: ($(that).offset().top - 48)
+    }, 800, 'swing')
+
+    // Firefox and IE
+    $('html').animate({
+      scrollTop: ($(that).offset().top - 34)
+    }, 800, 'swing')
+  }
 
   var appRouter = new AppRouter()
 
   $(function() {
-    console.log('start backbone history')
     Backbone.history.start({pushState: false})
   })
 })(jQuery, window);
